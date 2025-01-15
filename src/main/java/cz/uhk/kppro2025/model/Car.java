c690087d4c9c746676c3175cf6842d7a1cdaf871
tazlaro <77499119+tazlaro@users.noreply.github.com>
2025-01-15 17:08:21 +0100

    cv2
    CRUD cars

---
+++ src/main/java/cz/uhk/kppro2025/model/Car.java
@@ -0,0 +1,40 @@
+package cz.uhk.kppro2025.model;
+
+public class Car {
+    private String spz;
+    private String color;
+    private float tankVolume;
+    private int numberOfSeats;
+
+    public String getSpz() {
+        return spz;
+    }
+
+    public void setSpz(String spz) {
+        this.spz = spz;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getTankVolume() {
+        return tankVolume;
+    }
+
+    public void setTankVolume(float tankVolume) {
+        this.tankVolume = tankVolume;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+}
