8d8d2b44a5d0f5bdaf7908076d485efa046c177e
tazlaro <77499119+tazlaro@users.noreply.github.com>
2025-01-15 16:24:54 +0100

    Initial commit

---
+++ src/main/java/cz/uhk/kppro2025/Kppro2025Application.java
@@ -0,0 +1,13 @@
+package cz.uhk.kppro2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kppro2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kppro2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/cz/uhk/kppro2025/Kppro2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uhk.kppro2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kppro2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
