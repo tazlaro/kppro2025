ec29b25520bd6006b38df240e577bea5f44da053
tazlaro <77499119+tazlaro@users.noreply.github.com>
2025-01-31 02:21:00 +0100

    changed DB init
    Kppro2025Application.java

--- src/main/java/cz/uhk/kppro2025/Kppro2025Application.java
+++ src/main/java/cz/uhk/kppro2025/Kppro2025Application.java
@@ -50,32 +50,33 @@ public class Kppro2025Application {
 //        }
 //    }
 
-    @Order(1)
-    @Bean
-    public CommandLineRunner demo() {
-        return args -> {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-            try {
-                demoAddUser("admin", "heslo", "ADMIN", "Admin", "Admin", sdf.parse("1990-01-01"));
-                demoAddUser("user", "heslo", "USER", "User", "User", sdf.parse("1990-01-01"));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        };
-    }
 
-    private void demoAddUser(String username, String password, String role, String firstName, String lastName, Date birthday) {
-        if (userService.findByUsername(username) == null) {
-            User user = new User();
-            user.setUsername(username);
-            user.setPassword(passwordEncoder.encode(password));
-            user.setRole(role);
-            user.setFirstName(firstName);
-            user.setLastName(lastName);
-            user.setBirthday(birthday);
-            userService.save(user);
-        }
-    }
+//    @Order(1)
+//    @Bean
+//    public CommandLineRunner demo() {
+//        return args -> {
+//            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//            try {
+//                demoAddUser("admin", "heslo", "ADMIN", "Admin", "Admin", sdf.parse("1990-01-01"));
+//                demoAddUser("user", "heslo", "USER", "User", "User", sdf.parse("1990-01-01"));
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        };
+//    }
+//
+//    private void demoAddUser(String username, String password, String role, String firstName, String lastName, Date birthday) {
+//        if (userService.findByUsername(username) == null) {
+//            User user = new User();
+//            user.setUsername(username);
+//            user.setPassword(passwordEncoder.encode(password));
+//            user.setRole(role);
+//            user.setFirstName(firstName);
+//            user.setLastName(lastName);
+//            user.setBirthday(birthday);
+//            userService.save(user);
+//        }
+//    }
 
     @Order(2)
     @Bean
@@ -84,6 +85,9 @@ public class Kppro2025Application {
             return args -> {};
         }
         return args -> {
+            // default club for ADMIN and USER
+            addClub(0, "Club Default", "Club", "Club", "99999");
+            // other clubs
             addClub(1, "Club A", "123 Main St", "Springfield", "12345");
             addClub(2, "Club B", "456 Elm St", "Shelbyville", "67890");
             addClub(3, "Club C", "789 Oak St", "Capital City", "11223");
@@ -166,17 +170,23 @@ public class Kppro2025Application {
 //        };
 
         // user_id 1 = ADMIN, user_id 2 = USER
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         return args -> {
-            addUser("user1", "password1", "USER", "FirstName1", "LastName1", new Date(), "Street 1", "City 1", "10001", 1L);
-            addUser("user2", "password2", "USER", "FirstName2", "LastName2", new Date(), "Street 2", "City 2", "10002", 1L);
-            addUser("user3", "password3", "USER", "FirstName3", "LastName3", new Date(), "Street 3", "City 3", "10003", 2L);
-            addUser("user4", "password4", "USER", "FirstName4", "LastName4", new Date(), "Street 4", "City 4", "10004", 2L);
-            addUser("user5", "password5", "USER", "FirstName5", "LastName5", new Date(), "Street 5", "City 5", "10005", 3L);
-            addUser("user6", "password6", "USER", "FirstName6", "LastName6", new Date(), "Street 6", "City 6", "10006", 3L);
-            addUser("user7", "password7", "USER", "FirstName7", "LastName7", new Date(), "Street 7", "City 7", "10007", 4L);
-            addUser("user8", "password8", "USER", "FirstName8", "LastName8", new Date(), "Street 8", "City 8", "10008", 4L);
-            addUser("user9", "password9", "USER", "FirstName9", "LastName9", new Date(), "Street 9", "City 9", "10009", 5L);
-            addUser("user10", "password10", "USER", "FirstName10", "LastName10", new Date(), "Street 10", "City 10", "10010", 5L);
+
+            // default users ADMIN and USER (in default club)
+            addUser("admin", "heslo", "ADMIN", "Admin", "Admin", sdf.parse("1990-01-01"), "Admin", "Admin", "99999", 1L);
+            addUser("user", "heslo", "USER", "User", "User", sdf.parse("1990-01-01"), "User", "User", "99999", 1L);
+            // other users
+            addUser("user1", "password1", "USER", "FirstName1", "LastName1", new Date(), "Street 1", "City 1", "10001", 2L);
+            addUser("user2", "password2", "USER", "FirstName2", "LastName2", new Date(), "Street 2", "City 2", "10002", 2L);
+            addUser("user3", "password3", "USER", "FirstName3", "LastName3", new Date(), "Street 3", "City 3", "10003", 3L);
+            addUser("user4", "password4", "USER", "FirstName4", "LastName4", new Date(), "Street 4", "City 4", "10004", 3L);
+            addUser("user5", "password5", "USER", "FirstName5", "LastName5", new Date(), "Street 5", "City 5", "10005", 4L);
+            addUser("user6", "password6", "USER", "FirstName6", "LastName6", new Date(), "Street 6", "City 6", "10006", 4L);
+            addUser("user7", "password7", "USER", "FirstName7", "LastName7", new Date(), "Street 7", "City 7", "10007", 5L);
+            addUser("user8", "password8", "USER", "FirstName8", "LastName8", new Date(), "Street 8", "City 8", "10008", 5L);
+            addUser("user9", "password9", "USER", "FirstName9", "LastName9", new Date(), "Street 9", "City 9", "10009", 6L);
+            addUser("user10", "password10", "USER", "FirstName10", "LastName10", new Date(), "Street 10", "City 10", "10010", 6L);
         };
 
     }
